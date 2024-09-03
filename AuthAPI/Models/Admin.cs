659f81f49c6e37e795fbdd562208c38fffffe6be
Adithya Dinesh <adithyadinesh33@gmail.com>
2024-09-03 22:28:16 +0530

    Modified Admin class

--- AuthAPI/Models/Admin.cs
+++ AuthAPI/Models/Admin.cs
@@ -1,6 +1,10 @@
 ﻿namespace AuthAPI.Models
 {
-    public class Admin : Planner
+    public class Admin
     {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
