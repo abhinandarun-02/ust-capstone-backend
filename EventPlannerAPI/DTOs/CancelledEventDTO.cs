bdad2b97c36379c0f73ea2e2d4783709bd95da74
Adithya.Dinesh <adithya.dinesh@ust.com>
2024-09-09 10:14:58 +0530

    feat: remove booking and cancelledEvents models

--- EventPlannerAPI/DTOs/CancelledEventDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace EventPlannerAPI.DTOs
-{
-    public class CancelledEventDTO
-    {
-        public DateTime BookedDate { get; set; }
-        public DateTime CancelledDate { get; set; }
-        public string? ReasonForCancel { get; set; }
-    }
-}
--- EventPlannerAPI/Mapping/MappingProfile.cs
+++ EventPlannerAPI/Mapping/MappingProfile.cs
@@ -9,19 +9,12 @@ namespace EventPlannerAPI.Mapping
         public MappingProfile()
         {
             // Map models to DTOs
-            CreateMap<Booking, BookingDTO>()
-                .ForMember(dest => dest.WeddingName, opt => opt.MapFrom(src => src.Wedding.Name));
-            CreateMap<CancelledEvent, CancelledEventDTO>();
             CreateMap<Catering, CateringDTO>();
             CreateMap<Photography, PhotographyDTO>();
             CreateMap<Venue, VenueDTO>();
             CreateMap<Wedding, WeddingDTO>();
 
             // Map DTOs to models
-            CreateMap<BookingDTO, Booking>()
-                .ForMember(dest => dest.Wedding, opt => opt.Ignore());
-            CreateMap<CancelledEventDTO, CancelledEvent>()
-                .ForMember(dest => dest.BookingId, opt => opt.Ignore());
             CreateMap<CateringDTO, Catering>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore());
             CreateMap<PhotographyDTO, Photography>()
