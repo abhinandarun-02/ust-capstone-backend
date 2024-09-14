c94f95b4ae7eb1f96d60be344b1aae5840cba37d
Abhinand.Arun <abhinand.arun@ust.com>
2024-09-15 02:52:51 +0530

    refactor: Add category field to ExpenseDTO

--- EventPlannerAPI/DTOs/ExpenseDTO.cs
+++ EventPlannerAPI/DTOs/ExpenseDTO.cs
@@ -7,5 +7,6 @@ public class ExpenseDTO
     public int Id { get; set; }
     public int WeddingId { get; set; }
     public required string Name { get; set; }
+    public required string Category { get; set; }
     public decimal Cost { get; set; }
 }
